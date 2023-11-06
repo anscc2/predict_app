a37b290ca04f163515c8805a9d5ee2cd2c5880b3
anscc2 <54715329+anscc2@users.noreply.github.com>
2023-11-06 20:33:42 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,6 +6,8 @@ import requests
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from keras.preprocessing.sequence import pad_sequences
 from sru import SRU
+import streamlit as st
+import predict
 
 url_word_index = 'https://raw.githubusercontent.com/anscc2/predict_app/main/data/word_index.json'
 response = requests.get(url_word_index)
@@ -94,3 +96,11 @@ def predict_tweet(text):
   return result
 
 
+st.title("Predicting Bully Tweet")
+st.header('App ini dibuat untuk memprediksi apakah sebuah tweet terindikasi sebagai bully atau tidak')
+
+text = st.text_area("Masukkan text")
+output = ""
+if st.button("Predict"):
+    output = predict_tweet(text)
+st.success(f"Hasil Prediksi {output}")
